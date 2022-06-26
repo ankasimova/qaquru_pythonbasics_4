7586a0cbb3b520c6547761e5ec51d80e99566f00
ankasimova <ak1722@yandex.ru>
2022-06-26 14:31:05 +0600

    Add correct naming

--- func_name.py
+++ func_name.py
@@ -3,21 +3,21 @@
 # Вызовите ее внутри функций, описанных ниже
 # Подсказка: Имя функции можно получить с помощью func.__name__
 
-def read_func_name(func_name, *args):
+def read_func_name_and_args(func_name, *args):
     func_name = func_name.__name__
     arguments = '; '.join(args)
     print(f'Function name: {func_name}\nArguments: {arguments if len(arguments) else "Zero arguments assigned"}')
 
 
 def open_browser(browser_name):
-    read_func_name(open_browser, browser_name)
+    read_func_name_and_args(open_browser, browser_name)
 
 
 def go_to_companyname_homepage(page_url):
-    read_func_name(go_to_companyname_homepage, page_url)
+    read_func_name_and_args(go_to_companyname_homepage, page_url)
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    read_func_name(find_registration_button_on_login_page, page_url, button_text)
+    read_func_name_and_args(find_registration_button_on_login_page, page_url, button_text)
 
 
