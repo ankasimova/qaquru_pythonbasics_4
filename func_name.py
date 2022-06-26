95eab648cc58e99dd1254fe43bdf12dbcc0d2cfa
ankasimova <ak1722@yandex.ru>
2022-06-26 14:21:07 +0600

    Add name check function

---
+++ func_name.py
@@ -0,0 +1,23 @@
+# Сделайте функцию, которая будет печатать
+# читаемое имя переданной ей функции и значений аргументов.
+# Вызовите ее внутри функций, описанных ниже
+# Подсказка: Имя функции можно получить с помощью func.__name__
+
+def read_func_name(func_name, *args):
+    func_name = func_name.__name__
+    arguments = '; '.join(args)
+    print(f'Function name: {func_name}\nArguments: {arguments if len(arguments) else "Zero arguments assigned"}')
+
+
+def open_browser(browser_name):
+    read_func_name(open_browser, browser_name)
+
+
+def go_to_companyname_homepage(page_url):
+    read_func_name(go_to_companyname_homepage, page_url)
+
+
+def find_registration_button_on_login_page(page_url, button_text):
+    read_func_name(find_registration_button_on_login_page, page_url, button_text)
+
+
